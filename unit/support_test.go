969f3ec9489a313392d121dff9b6f4a167c09592
Brett Buddin <brett@buddin.us>
2017-11-17 18:26:37 -0500

    Some tests for unit.Unit

---
+++ unit/support_test.go
@@ -0,0 +1,66 @@
+package unit
+
+type noopSampleProc struct{}
+
+func (p noopSampleProc) ProcessSample(i int) {}
+
+type frameProcessor struct {
+	frame  func(int)
+	sample func(int)
+}
+
+func (p frameProcessor) ProcessFrame(n int) {
+	if p.frame != nil {
+		p.frame(n)
+	}
+}
+
+func (p frameProcessor) ProcessSample(i int) {
+	if p.sample != nil {
+		p.sample(i)
+	}
+}
+
+type sampleProcessor struct {
+	fn func(int)
+}
+
+func (p sampleProcessor) ProcessSample(i int) {
+	if p.fn != nil {
+		p.fn(i)
+	}
+}
+
+type closer struct {
+	fn func() error
+}
+
+func (c closer) Close() error {
+	if c.fn != nil {
+		return c.fn()
+	}
+	return nil
+}
+
+type outProcessor struct {
+	fn  func(int)
+	out *Out
+}
+
+func (p outProcessor) Out() *Out { return p.out }
+
+func (p outProcessor) ProcessSample(i int) {
+	if p.fn != nil {
+		p.fn(i)
+	}
+}
+
+type sampleProcessorCloser struct {
+	sampleProcessor
+	closer
+}
+
+type outProcessorCloser struct {
+	closer
+	outProcessor
+}
--- unit/unit_test.go
+++ unit/unit_test.go
@@ -1,1 +1,156 @@
 package unit
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"buddin.us/shaden/dsp"
+	"buddin.us/shaden/graph"
+)
+
+var namePattern = regexp.MustCompile("\\w+")
+
+func TestRegisteredNames(t *testing.T) {
+	for k := range Builders() {
+		require.True(t, namePattern.Match([]byte(k)))
+	}
+}
+
+func TestUnit_GraphAttachment(t *testing.T) {
+	io := NewIO()
+	io.NewIn("in", dsp.Float64(0))
+	io.NewOut("out")
+
+	u := NewUnit(io, "example", nil)
+	g := graph.New()
+	require.Equal(t, 0, g.Size())
+
+	err := u.Attach(g)
+	require.NoError(t, err)
+	require.Equal(t, 3, g.Size())
+	require.Len(t, u.node.InNeighbors(), 1)
+	require.Len(t, u.node.OutNeighbors(), 1)
+
+	err = u.Detach(g)
+	require.NoError(t, err)
+	require.Equal(t, 0, g.Size())
+}
+
+func TestUnit_Processable(t *testing.T) {
+	var (
+		io = NewIO()
+		u  = NewUnit(io, "example", noopSampleProc{})
+		g  = graph.New()
+	)
+	require.False(t, u.IsProcessable())
+	err := u.Attach(g)
+	require.NoError(t, err)
+	require.True(t, u.IsProcessable())
+}
+
+func TestUnit_NotProcessable(t *testing.T) {
+	var (
+		io = NewIO()
+		u  = NewUnit(io, "example", nil)
+		g  = graph.New()
+	)
+	require.False(t, u.IsProcessable())
+	err := u.Attach(g)
+	require.NoError(t, err)
+	require.False(t, u.IsProcessable())
+}
+
+func TestUnit_ProxyProcessFrame(t *testing.T) {
+	var (
+		recorded int
+
+		io        = NewIO()
+		processor = frameProcessor{frame: func(n int) { recorded = n }}
+		u         = NewUnit(io, "example", processor)
+		g         = graph.New()
+	)
+	err := u.Attach(g)
+	require.NoError(t, err)
+	u.ProcessFrame(10)
+	require.Equal(t, 10, recorded)
+}
+
+func TestUnit_ProcessFrameHonorRateSetting(t *testing.T) {
+	var (
+		sampleProcCalled bool
+
+		io        = NewIO()
+		processor = sampleProcessor{fn: func(i int) {
+			require.Equal(t, 0, i)
+		}}
+		u = NewUnit(io, "example", processor)
+		g = graph.New()
+	)
+	u.rate = RateControl
+	err := u.Attach(g)
+	require.NoError(t, err)
+	u.ProcessFrame(10)
+	require.False(t, sampleProcCalled)
+}
+
+func TestUnit_ExternalNeighborCount(t *testing.T) {
+	g := graph.New()
+
+	io1 := NewIO()
+	io1.NewIn("in", dsp.Float64(0))
+	io1.NewOut("out")
+	u1 := NewUnit(io1, "example1", nil)
+	require.Equal(t, 0, u1.ExternalNeighborCount())
+
+	io2 := NewIO()
+	io2.NewIn("in", dsp.Float64(0))
+	io2.NewOut("out")
+	u2 := NewUnit(io2, "example2", nil)
+	require.Equal(t, 0, u2.ExternalNeighborCount())
+
+	err := u1.Attach(g)
+	require.NoError(t, err)
+	err = u2.Attach(g)
+	require.NoError(t, err)
+
+	err = Patch(g, u1.Out["out"], u2.In["in"])
+	require.NoError(t, err)
+
+	require.Equal(t, 1, u1.ExternalNeighborCount())
+	require.Equal(t, 1, u2.ExternalNeighborCount())
+
+	err = Unpatch(g, u2.In["in"])
+	require.Equal(t, 0, u1.ExternalNeighborCount())
+	require.Equal(t, 0, u2.ExternalNeighborCount())
+}
+
+func TestUnit_Close(t *testing.T) {
+	var closeCalled int
+
+	io := NewIO()
+	io.NewIn("in", dsp.Float64(0))
+	out := NewOut("out", newFrame())
+	io.ExposeOutProcessor(outProcessorCloser{
+		closer: closer{
+			fn: func() error {
+				closeCalled++
+				return nil
+			},
+		},
+		outProcessor: outProcessor{out: out},
+	})
+
+	processor := sampleProcessorCloser{
+		closer: closer{fn: func() error {
+			closeCalled++
+			return nil
+		}},
+	}
+	u := NewUnit(io, "example1", processor)
+
+	err := u.Close()
+	require.NoError(t, err)
+	require.Equal(t, 2, closeCalled)
+}
