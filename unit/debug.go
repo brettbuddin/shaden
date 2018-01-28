6b3d570dc64b1e3b01a39b28bce8314f9df95da8
Brett Buddin <brett@buddin.us>
2018-01-28 13:11:21 -0500

    unit/debug improvements.
    - Only print when we have a new value within the sample rate.
    - Control over the sample rate as a percentage of the audio sample rate.

--- unit/debug.go
+++ unit/debug.go
@@ -13,22 +13,31 @@ func newDebug(name string, _ Config) (*Unit, error) {
 			p.value = v
 			return nil
 		}),
-		in:  io.NewIn("in", dsp.Float64(0)),
-		out: io.NewOut("out"),
+		in:   io.NewIn("in", dsp.Float64(0)),
+		rate: io.NewIn("rate", dsp.Float64(0.1)),
+		out:  io.NewOut("out"),
 	}), nil
 }
 
 type debug struct {
-	fmt  *Prop
-	in   *In
-	out  *Out
-	tick int
+	fmt      *Prop
+	in, rate *In
+	out      *Out
+	tick     int
+	lastIn   float64
 }
 
 func (d *debug) ProcessSample(i int) {
-	in := d.in.Read(i)
-	if d.tick%int(dsp.SampleRate*0.1) == 0 {
-		fmt.Printf(d.fmt.Value().(string)+"\n", in)
+	var (
+		in   = d.in.Read(i)
+		rate = dsp.Clamp(d.rate.Read(i), 0.01, 1)
+	)
+
+	if d.tick%int(dsp.SampleRate*rate) == 0 {
+		if d.lastIn != in {
+			fmt.Printf(d.fmt.Value().(string)+"\n", in)
+			d.lastIn = in
+		}
 		d.tick = 0
 	}
 	d.tick++
