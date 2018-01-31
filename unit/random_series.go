231738a1ab2ca970459e1e95f213bb23e4d92f7d
Brett Buddin <brett@buddin.us>
2018-01-30 21:39:08 -0500

    Old unit/random-series was less useful. Renamed unit/probability-series.

--- unit/random_series.go
+++ unit/random_series.go
@@ -1,69 +1,72 @@
 package unit
 
 import (
+	"math"
 	"math/rand"
 
 	"buddin.us/shaden/dsp"
 )
 
-const maxRandomSeriesSize = 64
-
 func newRandomSeries(name string, _ Config) (*Unit, error) {
 	io := NewIO()
 	return NewUnit(io, name, &randomSeries{
-		clock:       io.NewIn("clock", dsp.Float64(-1)),
-		size:        io.NewIn("size", dsp.Float64(8)),
-		trigger:     io.NewIn("trigger", dsp.Float64(-1)),
-		min:         io.NewIn("min", dsp.Float64(0)),
-		max:         io.NewIn("max", dsp.Float64(1)),
-		gate:        io.NewOut("gate"),
-		value:       io.NewOut("value"),
-		valMemory:   make([]float64, maxRandomSeriesSize),
-		gateMemory:  make([]float64, maxRandomSeriesSize),
-		lastTrigger: -1,
-		lastClock:   -1,
+		clock:     io.NewIn("clock", dsp.Float64(-1)),
+		length:    io.NewIn("length", dsp.Float64(8)),
+		lock:      io.NewIn("lock", dsp.Float64(0)),
+		min:       io.NewIn("min", dsp.Float64(0)),
+		max:       io.NewIn("max", dsp.Float64(1)),
+		gates:     make([]float64, 16),
+		values:    make([]float64, 16),
+		gate:      io.NewOut("gate"),
+		value:     io.NewOut("value"),
+		lastClock: -1,
 	}), nil
 }
 
 type randomSeries struct {
-	clock, size, trigger, min, max *In
-	gate, value                    *Out
-	valMemory, gateMemory          []float64
+	clock, lock, length, min, max *In
+	gate, value                   *Out
+	gates, values                 []float64
 
-	idx                    int
-	lastTrigger, lastClock float64
+	idx       int
+	lastClock float64
 }
 
 func (s *randomSeries) ProcessSample(i int) {
-	size := dsp.Clamp(s.size.Read(i), 1, maxRandomSeriesSize)
-	clock := s.clock.Read(i)
-	trigger := s.trigger.Read(i)
-	min := s.min.Read(i)
-	max := s.max.Read(i)
+	var (
+		clock     = s.clock.Read(i)
+		min       = s.min.Read(i)
+		max       = s.max.Read(i)
+		length    = dsp.Clamp(s.length.ReadSlow(i, ident), 2, 16)
+		lengthInt = int(length)
+		lock      = s.lock.ReadSlow(i, ident)
+	)
 
-	if s.lastClock < 0 && clock > 0 {
-		s.idx++
-		if s.idx >= int(size) {
-			s.idx = 0
+	if isTrig(s.lastClock, clock) {
+		var (
+			lastGate, lastValue = s.gates[lengthInt-1], s.values[lengthInt-1]
+			data                = rand.Float64()
+		)
+		for i := 0; i < lengthInt; i++ {
+			s.gates[i], lastGate = lastGate, s.gates[i]
+			s.values[i], lastValue = lastValue, s.values[i]
+		}
+		if lock != 1 && data > lock {
+			s.gates[0] = round(data)
+			s.values[0] = dsp.Lerp(min, max, rand.Float64())
 		}
 	}
-	if s.lastTrigger < 0 && trigger > 0 {
-		for j := 0; j < int(size); j++ {
-			s.valMemory[j] = dsp.Lerp(min, max, rand.Float64())
-			if rand.Float32() > 0.25 {
-				s.gateMemory[j] = 1
-			} else {
-				s.gateMemory[j] = -1
-			}
-		}
-	}
-	s.lastTrigger = trigger
+
+	s.gate.Write(i, s.gates[lengthInt-1])
+	s.value.Write(i, s.values[lengthInt-1])
+
 	s.lastClock = clock
+}
 
-	s.value.Write(i, s.valMemory[s.idx])
-	if s.gateMemory[s.idx] > 0 {
-		s.gate.Write(i, clock)
-	} else {
-		s.gate.Write(i, -1)
+func round(v float64) float64 {
+	t := math.Trunc(v)
+	if math.Abs(v-t) >= 0.5 {
+		return t + math.Copysign(1, v)
 	}
+	return t
 }
