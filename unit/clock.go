5eb1f87ff3237756f0be02e9979c67a4d4c60dc1
Brett Buddin <brett@buddin.us>
2018-02-14 12:02:35 -0500

    Allow clock shuffle/swing to go negative.

--- unit/clock.go
+++ unit/clock.go
@@ -23,12 +23,15 @@ type clock struct {
 	even bool
 }
 
+func clockShuffle(v float64) float64 { return dsp.Clamp(v, -0.5, 0.5) }
+
 func (c *clock) ProcessSample(i int) {
 	var (
-		pw      = c.pw.Read(i)
-		shuffle = dsp.Clamp(c.shuffle.Read(i), 0, 0.5)
-		tempo   = c.tempo.Read(i)
+		pw      = c.pw.ReadSlow(i, ident)
+		shuffle = c.shuffle.ReadSlow(i, clockShuffle)
+		tempo   = c.tempo.ReadSlow(i, ident)
 		duty    = 1 / (tempo * dsp.SampleRate) * dsp.SampleRate
+		offset  = duty * shuffle
 	)
 
 	if c.run.Read(i) <= 0 {
@@ -36,20 +39,30 @@ func (c *clock) ProcessSample(i int) {
 		return
 	}
 
-	if c.even {
-		if c.tick >= int(duty+(duty*shuffle)) {
-			c.tick = 0
-			c.even = false
-		}
-	} else if c.tick >= int(duty-(duty*shuffle)) {
-		c.tick = 0
-		c.even = true
+	if !c.even {
+		offset *= -1
 	}
 
+	c.advance(i, pw, duty+offset)
+}
+
+func (c *clock) advance(i int, pw, duty float64) {
+	if c.tick < int(duty) {
+		c.write(i, pw, duty)
+		c.tick++
+		return
+	}
+
+	c.tick = 0
+	c.even = !c.even
+	c.write(i, pw, duty)
+	c.tick++
+}
+
+func (c *clock) write(i int, pw, duty float64) {
 	if float64(c.tick) <= pw*duty {
 		c.out.Write(i, 1)
 	} else {
 		c.out.Write(i, -1)
 	}
-	c.tick++
 }
