93619fbeaef2651993a51b5abfba11356b1f746a
Brett Buddin <b@buddin.us>
2018-04-17 18:05:19 -0400

    Rename the inputs of adjust.

--- unit/adjust.go
+++ unit/adjust.go
@@ -4,23 +4,23 @@ import "buddin.us/shaden/dsp"
 
 func newAdjust(io *IO, _ Config) (*Unit, error) {
 	return NewUnit(io, &adjust{
-		in:     io.NewIn("in", dsp.Float64(0)),
-		gain:   io.NewIn("gain", dsp.Float64(1)),
-		offset: io.NewIn("offset", dsp.Float64(0)),
-		out:    io.NewOut("out"),
+		in:   io.NewIn("in", dsp.Float64(0)),
+		mult: io.NewIn("mult", dsp.Float64(1)),
+		add:  io.NewIn("add", dsp.Float64(0)),
+		out:  io.NewOut("out"),
 	}), nil
 }
 
 type adjust struct {
-	in, gain, offset *In
-	out              *Out
+	in, mult, add *In
+	out           *Out
 }
 
 func (a *adjust) ProcessSample(i int) {
 	var (
 		in   = a.in.Read(i)
-		mult = a.gain.Read(i)
-		add  = a.offset.Read(i)
+		mult = a.mult.Read(i)
+		add  = a.add.Read(i)
 	)
 	a.out.Write(i, in*mult+add)
 }
