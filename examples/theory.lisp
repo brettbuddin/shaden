950c393c8f06a2e3b8e4071e6744d1b0926d340c
Brett Buddin <b@buddin.us>
2018-08-18 13:41:22 -0400

    Clean up theory example.

--- examples/theory.lisp
+++ examples/theory.lisp
@@ -1,27 +1,24 @@
-; Reverse a list
-(define (reverse lst)
-  (define (reverse-accum lst accum)
-    (if (empty? lst) accum
-      (reverse-accum (rest lst) (cons (first lst) accum))))
-  (reverse-accum lst nil))
+; Shift pitches up a half step.
+(define (shift-semitone pitches)
+  (map (fn (_ p)
+           (theory/transpose p (theory/interval :semitone 1)))
+       pitches))
 
-; Shift pitches up a half step
-(define (shift _ p) 
-  (theory/transpose p (theory/interval :semitone 1)))
-
+; Interleave two lists.
 (define (interleave x y)
   (if (empty? x) y
     (cons (first x)
           (interleave y (rest x)))))
 
-; Ascending and descending whole-tone scales
+; Create the two whole-tone scales for this key; one ascending and the other
+; descending.
 (define root (theory/pitch "Eb4"))
 (define asc (theory/scale root :whole-tone 1))
-(define desc (map shift (reverse asc)))
+(define desc (shift-semitone (reverse asc)))
 
-; Interleave the ascending and descending lists and convert to Hz
-(define freqs 
-  (map (fn (_ p) (hz p)) 
+; Interleave the ascending and descending lists and convert to Hz.
+(define freqs
+  (map (fn (_ p) (hz p))
        (interleave asc desc)))
 
 (define clock (unit/clock))
@@ -30,15 +27,21 @@
 (define slope (unit/slope))
 (define gate (unit/gate))
 
-(-> clock (table :tempo (hz 5)))
-(-> switch (table :trigger (<- clock)) freqs)
+(-> clock (table :tempo (bpm 5)))
+
+(-> switch
+    freqs
+    (table :trigger (<- clock)))
+
 (-> gen (table :freq (<- switch)))
 
-(-> slope (table :trigger (<- clock)
-                 :rise (ms 10)
-                 :fall (ms 300)))
+(-> slope
+    (table :trigger (<- clock)
+           :rise (ms 10)
+           :fall (ms 300)))
 
-(-> gate (table :in (<- gen :triangle)
-                :control (<- slope)))
+(-> gate
+    (table :in (<- gen :triangle)
+           :control (<- slope)))
 
 (emit (<- gate))
