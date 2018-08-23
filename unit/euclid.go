a623040749b69a4a9b669d78e531a7951944a3d1
Brett Buddin <b@buddin.us>
2018-08-22 20:00:06 -0400

    Reset for euclid

--- unit/euclid.go
+++ unit/euclid.go
@@ -12,6 +12,7 @@ func newEuclid(io *IO, _ Config) (*Unit, error) {
 		span:        io.NewIn("span", dsp.Float64(5)),
 		fill:        io.NewIn("fill", dsp.Float64(2)),
 		offset:      io.NewIn("offset", dsp.Float64(0)),
+		reset:       io.NewIn("reset", dsp.Float64(-1)),
 		counts:      make([]int, maxEuclidLayers),
 		remainders:  make([]int, maxEuclidLayers),
 		pattern:     make([]bool, maxEuclidLayers),
@@ -21,9 +22,9 @@ func newEuclid(io *IO, _ Config) (*Unit, error) {
 }
 
 type euclid struct {
-	clock, span, fill, offset *In
-	lastTrigger               float64
-	lastSpan, lastFill        int
+	clock, span, fill, offset, reset *In
+	lastTrigger, lastReset           float64
+	lastSpan, lastFill               int
 
 	out *Out
 
@@ -38,6 +39,7 @@ func (e *euclid) ProcessSample(i int) {
 		fill   = e.fill.ReadSlowInt(i, identInt)
 		offset = e.offset.ReadSlowInt(i, modInt(span))
 		trig   = e.clock.Read(i)
+		reset  = e.reset.Read(i)
 		out    = -1.0
 	)
 
@@ -53,6 +55,8 @@ func (e *euclid) ProcessSample(i int) {
 
 		if isTrig(e.lastTrigger, trig) {
 			e.idx = (e.idx + 1) % span
+		} else if isTrig(e.lastReset, reset) {
+			e.idx = 0
 		}
 		idx := (e.idx + offset + span) % span
 		if e.pattern[idx] && e.idx == e.lastIdx {
@@ -64,6 +68,7 @@ func (e *euclid) ProcessSample(i int) {
 	e.lastFill = fill
 	e.lastIdx = e.idx
 	e.lastTrigger = trig
+	e.lastReset = reset
 
 	e.out.Write(i, out)
 }
