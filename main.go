1bbf7d60f6086263c535cb9acfac66b51e8fedb0
Brett Buddin <b@buddin.us>
2018-08-24 20:30:45 -0400

    Register pprof handlers.

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"math"
 	"math/rand"
 	"net/http"
-	_ "net/http/pprof"
+	"net/http/pprof"
 	"os"
 	"os/signal"
 	"syscall"
@@ -114,6 +114,17 @@ func run(cfg Config) error {
 	// Start the HTTP server
 	go func() {
 		mux := http.NewServeMux()
+
+		mux.HandleFunc("/debug/pprof/", pprof.Index)
+		mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+		mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+		mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+		mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+		mux.Handle("/debug/pprof/block", pprof.Handler("block"))
+		mux.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
+		mux.Handle("/debug/pprof/heap", pprof.Handler("heap"))
+		mux.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
+
 		runtime.AddHandler(mux, run)
 		if err := http.ListenAndServe(cfg.HTTPAddr, mux); err != nil {
 			logger.Fatal(err)
